9266324dc31e4fabc24a166e716003488813bc08
pooja kumari <152692774+pkakrk00@users.noreply.github.com>
2024-01-06 23:16:32 +0530

    multiplication code

---
+++ multiplication.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class multiplication {
+   public static void main(String[] args) {
+System.out.print("enter any number ") ;
+ Scanner sc = new Scanner(System.in);
+
+  int  n = sc.nextInt();
+for(int i=1; i<=10; i++)
+{
+    System.out.println(n + "*" + i + "=" + n*i);
+}
+ } 
+}
